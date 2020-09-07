6a888d8104f94d1e2188aba1afd3d103e368ddae
SansNeal <62467577+SansNeal@users.noreply.github.com>
2020-09-07 16:57:38 +0530

    Add files via upload

---
+++ screens/RequstersScreen.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Modal,
+  KeyboardAvoidingView,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+  ScrollView,
+} from "react-native";
+
+import db from "../config";
+
+export default class RequstersScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Book Requsters Screen</Text>
+      </View>
+    );
+  }
+}
